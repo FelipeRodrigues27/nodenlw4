fbfd4a3a1de9e440e0b258e30377d492b353a470
Felipe Rodrigues <frodrigues27.fr@gmail.com>
2021-02-25 20:35:41 -0300

    first commit 25/02/2021

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { getCustomRepository, getRepository } from "typeorm";
+import { User } from "../models/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UserController {
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        })
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                error: "User already exists!",
+            });
+        }
+
+        const user = usersRepository.create({
+            email,
+        });
+
+        await usersRepository.save(user);
+
+        return response.json(user);
+    }
+
+}
+export { UserController };
